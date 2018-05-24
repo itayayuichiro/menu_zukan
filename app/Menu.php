af454f964ea13f174f0ec25420093666ed7c2e6d
Yuichiro Itaya <mr.itaya.magic@gmail.com>
2018-05-24 17:40:30 +0900

    add: カラム返却/検索メソッドの追加

--- app/Menu.php
+++ app/Menu.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     protected $fillable = ['title', 'description', 'price', 'image_base64'];
+
     //
+    public static function publishableFields()
+    {
+        return array_merge(['id'], (new static)->fillable);
+    }
+
+    public static function searchByKeyword($keyword)
+    {
+        return Menu::where('title', 'LIKE', "%" . $keyword . "%")->orWhere('description', 'LIKE', "%" . $keyword . "%")->select(Menu::publishableFields())->get();
+    }
+
 }
